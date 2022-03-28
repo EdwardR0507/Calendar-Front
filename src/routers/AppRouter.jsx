5ef018813796ee3901f3defdb94c010fd9fe1ec3
EdwardR0507 <ramosedward21@gmail.com>
2022-03-27 23:28:04 -0500

    feat: public and private routers

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,24 +1,44 @@
 import { lazy, Suspense, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { startChecking } from "../actions/auth";
 
 const AuthRouter = lazy(() => import("./AuthRouter"));
 const MyCalendar = lazy(() => import("../pages/calendar/Index"));
-
+const PublicRouter = lazy(() => import("./PublicRouter"));
+const PrivateRouter = lazy(() => import("./PrivateRouter"));
 const AppRouter = () => {
   const dispatch = useDispatch();
+  const { checking, uid } = useSelector((state) => state.auth);
 
   useEffect(() => {
     dispatch(startChecking());
   }, [dispatch]);
 
+  if (checking) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/auth/*" element={<AuthRouter />} />
-          <Route path="/" element={<MyCalendar />} />
+          <Route
+            path="/auth/*"
+            element={
+              <PublicRouter isAuth={!!uid}>
+                <AuthRouter />
+              </PublicRouter>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <PrivateRouter isAuth={!!uid}>
+                <MyCalendar />
+              </PrivateRouter>
+            }
+          />
           <Route path="*" element={<Navigate to="/auth/login" />} />
         </Routes>
       </Suspense>
---
+++ src/routers/PublicRouter.jsx
@@ -0,0 +1,6 @@
+import { Navigate } from "react-router-dom";
+const PublicRouter = ({ isAuth, children }) => {
+  return !isAuth ? children : <Navigate to="/" />;
+};
+
+export default PublicRouter;
